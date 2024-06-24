0cb51b7b8e37d007ae8f478cd9117242ed411410
thiago <thiago@debian>
2024-06-24 19:26:01 -0300

    Simpify

--- arqref-one-web-security/src/main/java/arqref/one/web/security/EnableArqRefOneWebSecurity.java
+++ arqref-one-web-security/src/main/java/arqref/one/web/security/EnableArqRefOneWebSecurity.java
@@ -8,13 +8,10 @@ import java.lang.annotation.Target;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import arqref.web.one.security.EnableArqRefOneSecurity;
-
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Configuration
 @ComponentScan("arqref.one.web.security")
-@EnableArqRefOneSecurity
 public @interface EnableArqRefOneWebSecurity {
 
 }
--- arqref-one-web-security/src/main/java/arqref/one/web/security/service/ArqRefUserDetailsService.java
+++
@@ -1,9 +0,0 @@
-package arqref.one.web.security.service;
-
-import arqref.one.web.security.domain.ArqRefUserDetails;
-
-public interface ArqRefUserDetailsService {
-    
-    ArqRefUserDetails loadUserByUsername(String principalName);
-
-}
